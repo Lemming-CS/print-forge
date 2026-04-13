1cc0d8637ad10289adc1628ddcef536369d8b120
Lemming-CS <220599209+Lemming-CS@users.noreply.github.com>
2026-04-13 14:52:45 +0600

    Add about page

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+
+export default function Page() {
+return (
+  <div className="p-4 flex justify-center align-center">
+    <h1 className="m-auto">
+      About page
+    </h1>
+  </div>
+
+);
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="p-4 text-white ">
-    Default page
+      <h1>
+        Default page
+      </h1>
     </div>
   );
 }
